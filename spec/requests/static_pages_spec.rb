8a1538e249ecb122c6404453dda9f292eca41183
Dick Welch <dick.p.welch@gmail.com>
2014-03-12 08:20:15 -0500

    Added static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,34 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Design for Quality'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Design for Quality FMEA')
+    end
+        it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Design for Quality FMEA App | Home")
+    end
+  end
+  describe "Help page" do
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('help')
+    end
+        it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Design for Quality FMEA App | Help")
+    end
+  end
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+      it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Design for Quality FMEA App | About Us")
     end
   end
 end
