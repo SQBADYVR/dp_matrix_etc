3e77c9bb37e31630c7af8d7e6c60750ead769d5f
Dick Welch <dick.p.welch@gmail.com>
2014-03-13 13:13:44 -0500

    Finished signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
    def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Design for Quality App"
       redirect_to @user
     else
